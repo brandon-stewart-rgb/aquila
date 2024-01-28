4eb7d21782fa664c9231168c959ebdfe6dc3605f
brandon-stewart-rgb <brandon-stewart@pixeltree.us>
2024-01-28 16:01:43 -0700

    nav html/php items

--- inc/classes/class-menus.php
+++ inc/classes/class-menus.php
@@ -35,10 +35,24 @@
          $locations = get_nav_menu_locations();
     
         //get object id by location
-        $menu_id = $locations[ $location];
+        $menu_id = $locations[ $location ];
       
         // not empty (id) return menu id else nothing
         return ! empty( $menu_id ) ? $menu_id : '';
+       }
+
+       // filter through array and select only child menu items
+       public function get_child_menu_items ( $menu_array, $parent_id ) {
+        $child_menus = [];
+        // check to see if it's not empty before running a foreach loop
+         if ( ! empty( $menu_array ) && is_array( $menu_array ) ) {
+            foreach ( $menu_array as $menu ) {
+                if ( intval( $menu->menu_item_parent ) === $parent_id ) {
+                    array_push( $child_menus, $menu );
+                }
+            }
+         }
+         return $child_menus;
        }
     
      
